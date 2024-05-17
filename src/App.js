7aaef36ee8d105a79bac989241152bdd64284467
bry7n <limabryan032004@gmail.com>
2024-05-16 23:11:17 -0300

    React First Code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,68 @@
-import React from "react";
+//REACT HOOKS => FERRAMENTAS AUXILIARES
 
-function App () {
-  const hello = "Hello React";
+import React, { useState, useRef } from "react";
+
+import People from "./assets/people.svg"
+
+import Arrow from "./assets/arrow.svg"
+
+import Trash from "./assets/trash.svg"
+
+import {
+  Container,
+  H1,
+  Image,
+  ContainerItems,
+  InputLabel,
+  Input,
+  Button,
+  User
+} from "./styles";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const inputName = useRef()
+  const inputAge = useRef()
+
+
+  function addNewUser(){
+   setUsers([... users,{id: Math.random(), name:inputName.current.value, age:inputAge.current.value}])
+    console.log(inputName.current.value)
+    console.log(inputAge.current.value)
+  }
+
+  function deleteUser (userId){
+    const newUsers = users.filter(user => user.id !== userId)
+    setUsers(newUsers);
+
+  }
+
 
   return (
-    
-    <h1>{hello}</h1>
+    <Container>
+      <Image alt="logo-imagem" src={People} />
+      <ContainerItems>
+        <H1>Olá!</H1>
 
+        <InputLabel>Nome</InputLabel>
+        <Input ref={inputName} placeholder ="Nome" />
+
+        <InputLabel>Idade</InputLabel>
+        <Input ref={inputAge} placeholder= "Idade"/>
+
+        <Button onClick={addNewUser}>Cadastrar <img alt="seta" src={Arrow} /></Button>
+
+        <ul>
+          { users.map(user => (
+            <User key={user.id}>
+             <p> {user.name}</p> <p> {user.age} </p>
+             <button onClick={()=> deleteUser(user.id)}>
+              <img alt= "trash" src={Trash}/></button>
+            </User>
+          ))}
+        </ul>
+      </ContainerItems>
+    </Container>
   );
 }
 
